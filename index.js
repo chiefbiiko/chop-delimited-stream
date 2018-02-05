0e2112b38196bcae71efb43d644acfd66f7822a5
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-02-05 18:34:49 +0100

    choppa

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var BUF419 = Buffer.from([ 0x00, 0x04, 0x01, 0x09, 0x04, 0x01, 0x09, 0x00 ])
 var ZBUF0 = Buffer.alloc(0)
 
 function Chop (delimiter, trailing) {
-  if (!(this instanceof Chop)) return new Chop(delimiter)
+  if (!(this instanceof Chop)) return new Chop(delimiter, trailing)
   Transform.call(this)
 
   this._delimiter = Buffer.isBuffer(delimiter) ? delimiter : BUF419
@@ -22,7 +22,7 @@ Chop.prototype._transform = function transform (chunk, _, next) {
   for (var i = 0, head = 0, tail = chunk.length - 1; i < chunk.length; i++) {
     if (chunk[i] === this._delimiter0 &&
         chunk.slice(i, i + this._delimiter.length).equals(this._delimiter)) {
-      this.push(Buffer.concat([ this._stash, chunk.slice(head, i)]))
+      this.push(Buffer.concat([ this._stash, chunk.slice(head, i) ]))
       this._stash = ZBUF0
       head = i + this._delimiter.length
     } else if (i === tail) {
--- test.js
+++ test.js
@@ -9,14 +9,151 @@ tape('default delimiter is 419', function (t) {
   t.end()
 })
 
-tape('chops', function (t) {
+tape('tidy chops', function (t) {
+
+  var choppa = chop()
+
+  var msgs = [ 'chop', 'this', 'buffer', 'up' ]
+  var actual = []
+  var expected = [ 'chop', 'this', 'buffer', 'up' ]
+
+  var input = msgs.map(function (piece, i) {
+    return Buffer.concat([ Buffer.from(piece), BUF419 ])
+  })
+
+  function ondata (chunk) {
+    actual.push(chunk.toString())
+  }
+
+  function onend () {
+   t.same(actual, expected, 'same arrays')
+   t.end()
+  }
+
+  choppa.on('data', ondata)
+  choppa.on('end', onend)
+
+  var pending = input.length
+
+  input.forEach(function (chunk) {
+    choppa.write(chunk)
+    if (!--pending) choppa.end()
+  })
+
+})
+
+tape('dirty chops', function (t) {
+
+  var choppa = chop()
+
+  var msgs = [ 'chop', 'that', 'buffer', 'up' ]
+  var actual = []
+  var expected = [ 'chop', 'thatbuffer', 'up' ]
+
+  var input = msgs.map(function (piece, i) {
+    if (i === 1) return Buffer.from(piece)
+    else return Buffer.concat([ Buffer.from(piece), BUF419 ])
+  })
+
+  function ondata (chunk) {
+    actual.push(chunk.toString())
+  }
+
+  function onend () {
+   t.same(actual, expected, 'same arrays')
+   t.end()
+  }
+
+  choppa.on('data', ondata)
+  choppa.on('end', onend)
+
+  var pending = input.length
+
+  input.forEach(function (chunk) {
+    choppa.write(chunk)
+    if (!--pending) choppa.end()
+  })
+
+})
+
+tape('endless chops', function (t) {
+
+  var choppa = chop() // arg trailing defaults to false
+
+  var msgs = [ 'chop', 'this', 'buffer', 'up' ]
+  var actual = []
+  var expected = [ 'chop', 'this', 'buffer' ]
+
+  var input = msgs.map(function (piece, i) {
+    if (i === msgs.length - 1) return Buffer.from(piece)
+    return Buffer.concat([ Buffer.from(piece), BUF419 ])
+  })
+
+  function ondata (chunk) {
+    actual.push(chunk.toString())
+  }
+
+  function onend () {
+   t.same(actual, expected, 'same arrays')
+   t.end()
+  }
+
+  choppa.on('data', ondata)
+  choppa.on('end', onend)
+
+  var pending = input.length
+
+  input.forEach(function (chunk) {
+    choppa.write(chunk)
+    if (!--pending) choppa.end()
+  })
+
+})
+
+tape('trailing chops', function (t) {
+
+  var choppa = chop(BUF419, true) // trailing=true
+
+  var msgs = [ 'chop', 'this', 'buffer', 'up' ]
+  var actual = []
+  var expected = [ 'chop', 'this', 'buffer', 'up' ]
+
+  var input = msgs.map(function (piece, i) {
+    if (i === msgs.length - 1) return Buffer.from(piece)
+    return Buffer.concat([ Buffer.from(piece), BUF419 ])
+  })
+
+  function ondata (chunk) {
+    actual.push(chunk.toString())
+  }
+
+  function onend () {
+   t.same(actual, expected, 'same arrays')
+   t.end()
+  }
+
+  choppa.on('data', ondata)
+  choppa.on('end', onend)
+
+  var pending = input.length
+
+  input.forEach(function (chunk) {
+    choppa.write(chunk)
+    if (!--pending) choppa.end()
+  })
+
+})
+
+tape('delimiter head chops', function (t) {
 
   var choppa = chop()
 
   var actual = []
+  var msgs = [ 'chop', 'this', 'buffer', 'up' ]
   var expected = [ 'chop', 'this', 'buffer', 'up' ]
 
-  var input = expected.map(function (piece, i) {
+  var input = msgs.map(function (piece, i) {
+    if (!i) return Buffer.concat([ BUF419, Buffer.from(piece), BUF419 ])
     return Buffer.concat([ Buffer.from(piece), BUF419 ])
   })
 
